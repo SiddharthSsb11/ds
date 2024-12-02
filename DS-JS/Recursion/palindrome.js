ffb5640af097ef2de2195e3744979ae06b730f86
SiddharthSsb11 <siddharthbhadoriya1@gmail.com>
2024-12-02 07:22:52 +0530

    ll

--- DS-JS/Recursion/palindrome.js
+++ DS-JS/Recursion/palindrome.js
@@ -14,3 +14,16 @@ const isPalindrome = (str) => {
 
 console.log(isPalindrome("22")); //true
 console.log(isPalindrome("14241")); //true
+
+const otherPalindrome = (str) => {
+  const n = str.length;
+  let left = 0;
+  let right = n - 1;
+  if (n === 0) return false;
+  while (left < right) {
+    if (str(left) !== str(right)) return false;
+    left++;
+    right--;
+  }
+  return true;
+};
